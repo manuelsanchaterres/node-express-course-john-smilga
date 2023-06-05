9997680bfe61e54a5620955500b2e5018e813112
mst736 <manuelsanchaterres@gmail.com>
2023-06-05 14:20:51 +0200

    section 3 node tutorial finished

--- 01-node-tutorial/2-async-patterns/1-block.js
+++ 01-node-tutorial/2-async-patterns/1-block.js
@@ -3,17 +3,26 @@ const http = require('http')
 const server = http.createServer((req, res) => {
   if (req.url === '/') {
     res.end('Home Page')
-  }
-  if (req.url === '/about') {
+  } 
+  
+  else if (req.url === '/about') {
     // blocking code
+    console.time("for loop")
     for (let i = 0; i < 1000; i++) {
       for (let j = 0; j < 1000; j++) {
         console.log(`${i} ${j}`)
       }
     }
+    console.timeEnd("for loop")
     res.end('About Page')
+  } 
+  
+  else {
+
+    res.end('Error Page')
   }
-  res.end('Error Page')
+
+
 })
 
 server.listen(5000, () => {
