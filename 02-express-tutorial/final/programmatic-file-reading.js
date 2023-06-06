31c4aa77dcee2f7fece4401bbebcefa5c23bb121
mst736 <manuelsanchaterres@gmail.com>
2023-06-06 13:08:28 +0200

    programmatic-file-reading.js file created to scrap directory files and serve them

--- 02-express-tutorial/final/programmatic-file-reading.js
+++ 02-express-tutorial/final/programmatic-file-reading.js
@@ -6,9 +6,13 @@ const { readDirectoryServeContentFiles } = require('./utils/functions');
 
 const server = http.createServer((req, res) => {
 
+    // on this array you can insert all directories path you need to read files from
+
     let routePaths= ['./navbar-app'];
 
     for ( let i = 0; i < routePaths.length; i++ ){
+
+        // this function reads directory files and writes on server response to serve them
 
         readDirectoryServeContentFiles(routePaths[i], readdir, readFile, path, req, res)
 
